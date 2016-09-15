84f65d42ed3062340a5689db37548fa8b03c19d0
Jan Ypma <jyp@tradeshift.com>
2016-09-15 09:27:25 +0200

    HttpFlow materializes into HttpResponse, instead of just Done
    This allows users of the flow to take additional actions on e.g. the
    content type of the response.

--- ts-reaktive-marshal-akka/src/test/java/com/tradeshift/reaktive/marshal/http/HttpFlowSpec.java
+++ ts-reaktive-marshal-akka/src/test/java/com/tradeshift/reaktive/marshal/http/HttpFlowSpec.java
@@ -5,12 +5,15 @@ import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.tradeshift.reaktive.assertj.CompletionStageAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.forgerock.cuppa.Cuppa.after;
 import static org.forgerock.cuppa.Cuppa.beforeEach;
 import static org.forgerock.cuppa.Cuppa.describe;
 import static org.forgerock.cuppa.Cuppa.it;
 
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
 
 import org.forgerock.cuppa.junit.CuppaRunner;
 import org.junit.runner.RunWith;
@@ -20,11 +23,13 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.tradeshift.reaktive.testkit.SharedActorSystemSpec;
 
-import akka.Done;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.ContentTypes;
 import akka.http.javadsl.model.HttpMethods;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.model.Uri;
+import akka.http.javadsl.model.headers.RawHeader;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
@@ -51,7 +56,7 @@ public class HttpFlowSpec extends SharedActorSystemSpec {{
             stubFor(put(urlMatching(".*")).willReturn(aResponse().withStatus(201).withBody("response")));
             
             HttpFlow httpFlow = new HttpFlow(Http.get(system), materializer);
-            Flow<ByteString, ByteString, CompletionStage<Done>> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:" + wireMockServer.port()), ContentTypes.APPLICATION_JSON);
+            Flow<ByteString, ByteString, ?> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:" + wireMockServer.port()), ContentTypes.APPLICATION_JSON);
             
             assertThat(
                 Source.single(ByteString.fromString("request"))
@@ -69,9 +74,26 @@ public class HttpFlowSpec extends SharedActorSystemSpec {{
             ).succeedsWith("iwashere");
         });
         
+        it("should materialize into an HttpResponse with the reponse status and headers", () -> {
+            stubFor(put(urlMatching(".*")).willReturn(aResponse().withStatus(201).withHeader("X-foo", "bar").withBody("response")));
+            
+            HttpFlow httpFlow = new HttpFlow(Http.get(system), materializer);
+            Flow<ByteString, ByteString, CompletionStage<HttpResponse>> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:" + wireMockServer.port()), ContentTypes.APPLICATION_JSON);
+            
+            HttpResponse resp = Source.single(ByteString.fromString("request"))
+                .viaMat(flow, (m1,m2) -> m2)
+                .toMat(Sink.ignore(), (m1,m2) -> m1)
+                .run(materializer)
+                .toCompletableFuture()
+                .get(1, TimeUnit.SECONDS);
+            
+            assertThat(resp.getHeader("X-foo")).isEqualTo(Optional.of(RawHeader.create("X-foo", "bar")));
+            assertThat(resp.status()).isEqualTo(StatusCodes.CREATED);
+        });
+        
         it("should fail the stream if the http request could not be made", () -> {
             HttpFlow httpFlow = new HttpFlow(Http.get(system), materializer);
-            Flow<ByteString, ByteString, CompletionStage<Done>> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:1"), ContentTypes.APPLICATION_JSON);
+            Flow<ByteString, ByteString, ?> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:1"), ContentTypes.APPLICATION_JSON);
             
             assertThat(
                 Source.single(ByteString.fromString("request"))
@@ -84,7 +106,7 @@ public class HttpFlowSpec extends SharedActorSystemSpec {{
             stubFor(put(urlMatching(".*")).willReturn(aResponse().withStatus(400).withBody("computer says no")));
             
             HttpFlow httpFlow = new HttpFlow(Http.get(system), materializer);
-            Flow<ByteString, ByteString, CompletionStage<Done>> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:" + wireMockServer.port()), ContentTypes.APPLICATION_JSON);
+            Flow<ByteString, ByteString, ?> flow = httpFlow.flow(HttpMethods.PUT, Uri.create("http://localhost:" + wireMockServer.port()), ContentTypes.APPLICATION_JSON);
             
             assertThat(
                 Source.single(ByteString.fromString("request"))
