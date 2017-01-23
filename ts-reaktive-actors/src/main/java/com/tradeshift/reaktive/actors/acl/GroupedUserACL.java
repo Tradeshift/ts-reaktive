6d924f471572d398d82f4918059cddb7b8f91768
Jan Ypma <jyp@tradeshift.com>
2017-01-23 12:56:11 +0100

    Remove special admin handling, it only confuses things.

--- ts-reaktive-actors/src/main/java/com/tradeshift/reaktive/actors/acl/GroupedUserACL.java
+++ ts-reaktive-actors/src/main/java/com/tradeshift/reaktive/actors/acl/GroupedUserACL.java
@@ -9,23 +9,19 @@ import javaslang.Function1;
 import javaslang.collection.HashSet;
 import javaslang.collection.Set;
 import javaslang.control.Option;
-import static javaslang.control.Option.*;
 
 /**
  * Combines an ACL for users and an ACL for groups.
  * 
  * This class implements a security mechanism where during security checks, an application is expected
  * to have the current users' ID, and all of it group memberships, cached in memory.
- * 
- * The class is aware of a special "admin" right. Users that have that right implicitly have all other rights as well.
  *
  * @param R Enumeration containing the various rights that can be granted on the resource
  * @param C Type of the change object that can be applied to update the ACL
  */
 public class GroupedUserACL<R extends Enum<R>,C> {
     /**
-     * Creates a new, empty, ACL, based on various lambda expressions for R and C, without defining a special "admin" right
-     * (meaning all rights are simply checked individually).
+     * Creates a new, empty, ACL, based on various lambda expressions for R and C.
      * @param rightsType The type of enum for which this ACL checks access rights
      * @param getUserId Function that yields a target user UUID for a change (or none() if that change doesn't target a user)
      * @param getUserGroupId Function that yields a target user group UUID for a change (or none() if that change doesn't target a user group)
@@ -39,35 +35,15 @@ public class GroupedUserACL<R extends Enum<R>,C> {
         Function1<C, Option<R>> getGranted,
         Function1<C, Option<R>> getRevoked
     ) {
-        return new GroupedUserACL<>(rightsType, none(), ACLBuilder.of(getUserId, getGranted, getRevoked).empty(), ACLBuilder.of(getUserGroupId, getGranted, getRevoked).empty());
+        return new GroupedUserACL<>(rightsType, ACLBuilder.of(getUserId, getGranted, getRevoked).empty(), ACLBuilder.of(getUserGroupId, getGranted, getRevoked).empty());
     }
     
-    /**
-     * Creates a new, empty, ACL, based on various lambda expressions for R and C, while defining a special "admin" right.
-     * @param admin Enumeration literal that is the special "admin" right for this ACL. Having this right grants all other rights.
-     * @param getUserId Function that yields a target user UUID for a change (or none() if that change doesn't target a user)
-     * @param getUserGroupId Function that yields a target user group UUID for a change (or none() if that change doesn't target a user group)
-     * @param getGranted Function that yields a right that was granted for a change (or none() if the change doesn't grant rights)
-     * @param getRevoked Function that yields a right that was revoked for a change (or none() if the change doesn't revoke rights)
-     */
-    public static <R extends Enum<R>,C> GroupedUserACL<R,C> empty(
-        R admin,
-        Function1<C, Option<UUID>> getUserId,
-        Function1<C, Option<UUID>> getUserGroupId,
-        Function1<C, Option<R>> getGranted,
-        Function1<C, Option<R>> getRevoked
-    ) {
-        return new GroupedUserACL<>(admin.getDeclaringClass(), some(admin), ACLBuilder.of(getUserId, getGranted, getRevoked).empty(), ACLBuilder.of(getUserGroupId, getGranted, getRevoked).empty());
-    }
-
     private final Class<R> rightsType;
-    private final Option<R> admin;
     private final ACL<R,C> userAcl;
     private final ACL<R,C> groupAcl;
     
-    private GroupedUserACL(Class<R> rightsType, Option<R> admin, ACL<R, C> userAcl, ACL<R, C> groupAcl) {
+    private GroupedUserACL(Class<R> rightsType, ACL<R, C> userAcl, ACL<R, C> groupAcl) {
         this.rightsType = rightsType;
-        this.admin = admin;
         this.userAcl = userAcl;
         this.groupAcl = groupAcl;
     }
@@ -76,26 +52,33 @@ public class GroupedUserACL<R extends Enum<R>,C> {
      * Returns a new ACL with the given change applied.
      */
     public GroupedUserACL<R,C> apply(C change) {
-        return new GroupedUserACL<>(rightsType, admin, userAcl.apply(change), groupAcl.apply(change));
+        return new GroupedUserACL<>(rightsType, userAcl.apply(change), groupAcl.apply(change));
     }
     
     /**
      * Returns whether a certain user, being a member of certain groups, can perform a certain right.
-     * If the user (or one of its groups) has the special "admin" right, this function always returns true.
      * @param user The user to check
      * @param userGroups Groups the user is in (in protobuf format, since that's typically where clustered apps will maintain them)
      * @param right The right to check
      * @return whether the user, or one of its groups, can perform that right
      */
-    public boolean isAllowed(UUID user, List<com.tradeshift.reaktive.protobuf.Types.UUID> userGroups, R right) {
-        if (userAcl.isAllowed(right, user) ||
-            userGroups.stream().map(UUIDs::toJava).anyMatch(group -> groupAcl.isAllowed(right, group))) {
-            return true;
-        } else if (admin.filter(r -> r != right).isDefined()) {
-            return isAllowed(user, userGroups, admin.get());
-        } else {
-            return false;
-        }
+    public boolean isGranted(UUID user, List<com.tradeshift.reaktive.protobuf.Types.UUID> userGroups, R right) {
+        return (userAcl.isGranted(right, user) ||
+                userGroups.stream().map(UUIDs::toJava).anyMatch(group -> groupAcl.isGranted(right, group)));
+    }
+    
+    /**
+     * Returns whether a certain user is directly granted a certain right (not taking user groups into account).
+     */
+    public boolean isGrantedToUser(UUID user, R right) {
+        return userAcl.isGranted(right, user);
+    }
+    
+    /**
+     * Returns whether a certain user group is directly granted a certain right.
+     */
+    public boolean isGrantedToGroup(UUID userGroup, R right) {
+        return groupAcl.isGranted(right, userGroup);
     }
     
     /**
@@ -108,7 +91,7 @@ public class GroupedUserACL<R extends Enum<R>,C> {
     public Set<R> getRights(UUID userId, List<com.tradeshift.reaktive.protobuf.Types.UUID> userGroups) {
         Set<R> set = HashSet.empty();
         for (R right: rightsType.getEnumConstants()) {
-            if (isAllowed(userId, userGroups, right)) {
+            if (isGranted(userId, userGroups, right)) {
                 set = set.add(right);
             }
         }
@@ -121,4 +104,19 @@ public class GroupedUserACL<R extends Enum<R>,C> {
     public boolean isEmpty() {
         return userAcl.isEmpty() && groupAcl.isEmpty();
     }
+    
+    /**
+     * Returns whether the given userId is the only (i.e. last) user that is granted that right (directly).
+     */
+    public boolean isOnlyGrantedToUser(UUID userId, R right) {
+        return userAcl.isOnlyGrantedTo(userId, right);
+    }
+
+    /**
+     * Returns whether the given groupId is the only (i.e. last) group that is granted that right (directly).
+     */
+    public boolean isOnlyGrantedToGroup(UUID groupId, R right) {
+        return groupAcl.isOnlyGrantedTo(groupId, right);
+    }
 }
+
