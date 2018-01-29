7dcff614a76c6454fd82ef40ce9312742393738a
Jan Ypma <jyp@tradeshift.com>
2018-01-29 14:24:01 +0100

    Make PersistentActorSharding less smart, and have impl decide on prefix
    By simplifying the lambda that goes into PersistentActorSharding,
    implementations can decide on their own rules of prefixing persistenceIds.
    This also isolates the code that does the reverse a lot better.

--- ts-reaktive-replication/src/main/java/com/tradeshift/reaktive/replication/actors/ReplicatedActorSharding.java
+++ ts-reaktive-replication/src/main/java/com/tradeshift/reaktive/replication/actors/ReplicatedActorSharding.java
@@ -14,29 +14,43 @@ import akka.actor.Props;
 public class ReplicatedActorSharding<C> extends PersistentActorSharding<C> {
 
     /**
-     * Creates a ReplicatedActorSharding for an actor that is created according to [props].
+     * Creates a ReplicatedActorSharding for an actor that is created according to [props]. The actor must be a subclass of {@link ReplicatedActor}.
      * Entities will be sharded onto 256 shards.
      * 
-     * @param persistenceIdPrefix Fixed prefix for each persistence id. This is typically the name of your aggregate root, e.g. "document" or "user".
-     * @param persistenceIdPostfix Function that returns the last part of the persistence id that a command is routed to. This typically is the real ID of your entity, or UUID.
+     * @param typeName    Actor name of the top-level shard region actor. This must be unique for each PersistentActorSharding type.
+     * @param props       Props that is used to instantiate child actors on shards
+     * @param getEntityId Function that returns the entityId (=persistenceId, and child actor name) that a command is routed to. 
+     *                    This can be a UUID or the aggregate ID of your entity, possibly prefixed with a fixed static string to differentiate it
+     *                    from other types in a shared journal.
      */
-    public static <C> ReplicatedActorSharding<C> of(Props props, String persistenceIdPrefix, Function<C, String> persistenceIdPostfix) {
-        return new ReplicatedActorSharding<>(props, persistenceIdPrefix, persistenceIdPostfix, 256);
+    public static <C> ReplicatedActorSharding<C> of(String typeName, Props props, Function<C, String> getEntityId) {
+        return new ReplicatedActorSharding<>(typeName, props, getEntityId, 256);
     }
     
     /**
-     * Creates a ReplicatedActorSharding for an actor that is created according to [props].
-     * 
+     * Creates a ReplicatedActorSharding for an actor that is created according to [props]. The actor must be a subclass of {@link ReplicatedActorS}.
+     *
+     * @param typeName    Actor name of the top-level shard region actor. This must be unique for each PersistentActorSharding type.
+     * @param props       Props that is used to instantiate child actors on shards
+     * @param getEntityId Function that returns the entityId (=persistenceId, and child actor name) that a command is routed to. 
+     *                    This can be a UUID or the aggregate ID of your entity, possibly prefixed with a fixed static string to differentiate it
+     *                    from other types in a shared journal.
      * @param numberOfShards Number of shards to divide all entity/persistence ids into. This can not be changed after the first run.
-     * @param persistenceIdPrefix Fixed prefix for each persistence id. This is typically the name of your aggregate root, e.g. "document" or "user".
-     * @param persistenceIdPostfix Function that returns the last part of the persistence id that a command is routed to. This typically is the real ID of your entity, or UUID.
      */
-    public static <C> ReplicatedActorSharding<C> of(Props props, String persistenceIdPrefix, Function<C, String> persistenceIdPostfix, int numberOfShards) {
-        return new ReplicatedActorSharding<>(props, persistenceIdPrefix, persistenceIdPostfix, numberOfShards);
+    public static <C> ReplicatedActorSharding<C> of(String typeName, Props props, Function<C, String> getEntityId, int numberOfShards) {
+        return new ReplicatedActorSharding<>(typeName, props, getEntityId, numberOfShards);
     }
     
-    protected ReplicatedActorSharding(Props props, String persistenceIdPrefix, Function<C, String> persistenceIdPostfix, int numberOfShards) {
-        super(props, persistenceIdPrefix, persistenceIdPostfix, numberOfShards);
+    /**
+     * @deprecated Use the variant where the lambda returns the whole whole entityId, since then the implementation is in full control.  
+     */
+    @Deprecated
+    public static <C> PersistentActorSharding<C> of(Props props, String persistenceIdPrefix, Function<C, String> persistenceIdPostfix) {
+        return of(persistenceIdPrefix, props, c -> persistenceIdPrefix + "_" + persistenceIdPostfix.apply(c));
+    }
+    
+    protected ReplicatedActorSharding(String typeName, Props props, Function<C, String> getEntityId, int numberOfShards) {
+        super(typeName, props, getEntityId, numberOfShards);
     }
 
     /**
