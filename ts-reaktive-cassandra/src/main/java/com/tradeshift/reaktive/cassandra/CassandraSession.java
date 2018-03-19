5ae2132195c08d7c78c449827185c120f93abe23
Ali Araghi <ara@tradeshift.com>
2018-03-19 11:57:50 +0100

    fixup! Add initial statements execution to CassandraSession

--- ts-reaktive-cassandra/src/main/java/com/tradeshift/reaktive/cassandra/CassandraSession.java
+++ ts-reaktive-cassandra/src/main/java/com/tradeshift/reaktive/cassandra/CassandraSession.java
@@ -4,6 +4,7 @@ import static com.tradeshift.reaktive.ListenableFutures.toJava;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Row;
@@ -16,7 +17,7 @@ import akka.actor.ActorSystem;
 import akka.persistence.cassandra.ConfigSessionProvider;
 import akka.persistence.cassandra.session.CassandraSessionSettings;
 import akka.stream.javadsl.Source;
-import io.vavr.collection.Vector;
+import io.vavr.collection.Seq;
 import io.vavr.control.Option;
 /**
  * Provides asynchronous, non-blocking access to a cassandra session.
@@ -24,19 +25,23 @@ import io.vavr.control.Option;
 public class CassandraSession {
     private final akka.persistence.cassandra.session.javadsl.CassandraSession delegate;
 
-    public CassandraSession(ActorSystem system, String metricsCategory, Vector<String> statements) {
+    public CassandraSession(ActorSystem system, String metricsCategory, Function<Session,CompletionStage<Done>> init) {
         this.delegate = new akka.persistence.cassandra.session.javadsl.CassandraSession(system,
             new ConfigSessionProvider(system, system.settings().config().getConfig("cassandra-journal")),
             new CassandraSessionSettings(system.settings().config().getConfig("cassandra-journal")),
-            system.dispatcher(), system.log(), metricsCategory, s -> createCassandraInit(s, statements));
+            system.dispatcher(), system.log(), metricsCategory, init);
     }
 
-    private static CompletionStage<Done> createCassandraInit(Session session, Vector<String> statements) {
+    public CassandraSession(ActorSystem system, String metricsCategory, Seq<String> initializationStatements) {
+        this(system, metricsCategory, s -> executeInitStatements(s, initializationStatements));
+    }
+
+    private static CompletionStage<Done> executeInitStatements(Session session, Seq<String> statements) {
         if (statements.isEmpty()) {
             return CompletableFuture.completedFuture(Done.getInstance());
         }
 
-        return toJava(session.executeAsync(statements.head())).thenCompose(rs -> createCassandraInit(session, statements.tail()));
+        return toJava(session.executeAsync(statements.head())).thenCompose(rs -> executeInitStatements(session, statements.tail()));
     }
 
     public CompletionStage<Session> getUnderlying() {
