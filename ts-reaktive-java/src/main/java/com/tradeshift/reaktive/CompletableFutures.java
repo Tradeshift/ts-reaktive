2871c9972cc602acd32c06ab804e45372e9660c0
Jan Ypma <jyp@tradeshift.com>
2019-06-19 09:14:26 +0200

    Allow CompletionStage to be directly used in CompletableFutures

--- ts-reaktive-java/src/main/java/com/tradeshift/reaktive/CompletableFutures.java
+++ ts-reaktive-java/src/main/java/com/tradeshift/reaktive/CompletableFutures.java
@@ -3,6 +3,7 @@ package com.tradeshift.reaktive;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
 
 import io.vavr.Value;
@@ -24,22 +25,23 @@ public class CompletableFutures {
     /**
      * Returns a future that completes when all the futures in the given collection have completed.
      */
-    public static <T,U extends T> CompletableFuture<List<T>> sequence(Collection<CompletableFuture<U>> futures) {
+    public static <T,U extends T> CompletableFuture<List<T>> sequence(Collection<? extends CompletionStage<U>> futures) {
         return sequence(futures.stream());
     }
     
     /**
      * Returns a future that completes when all the futures in the given stream have completed.
      */
-    public static <T,U extends T> CompletableFuture<List<T>> sequence(Stream<CompletableFuture<U>> futures) {
+    public static <T,U extends T> CompletableFuture<List<T>> sequence(Stream<? extends CompletionStage<U>> futures) {
         return CompletableFutures.<T,U>sequence(futures.collect(Vector.collector())).thenApply(v -> v.toJavaList());
     }
     
     /**
      * Returns a future that completes when all the futures in the given collection have completed.
      */
-    public static <T,U extends T> CompletableFuture<Vector<T>> sequence(Value<CompletableFuture<U>> futures) {
+    public static <T,U extends T> CompletableFuture<Vector<T>> sequence(Value<? extends CompletionStage<U>> completionStages) {
         CompletableFuture<Vector<T>> result = new CompletableFuture<>();
+        Value<CompletableFuture<U>> futures = completionStages.map(f -> f.toCompletableFuture());
 
         // We don't join() the futures until allOf() has completed, since otherwise we'll be blocking
         // a thread on the common thread pool all the time we're waiting
